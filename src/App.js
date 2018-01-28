9e65e64e509376cebc445547158735eb919c026a
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-01-28 12:42:35 -0800

    Build contacts UI with ListContacts component and test with sample data without API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
+
+// temporary contact files
+// will be replaced when the server is built
+const contacts = [
+  {
+    id: 'brutus',
+    name: 'Brutus Burrito',
+    email: 'brutus@brutusburrito.com',
+    avatarURL: '/brutus.jpg'
+  },
+  {
+    id: 'smudge',
+    name: 'Purrfect Smudge',
+    email: 'smudge@purrfectsmudge.com',
+    avatarURL: '/smudge.jpg'
+  }
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Test</h1>
         </header>
+        <div>
+          <ListContacts contacts={contacts} />
+        </div>
       </div>
     );
   }
